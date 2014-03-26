62d0b5f569d900bf450ba3999be8da64dfa4a6d0
HARSHA S RAIKAR <hraikar@live.com>
2014-03-27 00:48:34 +0530

    Adding code

---
+++ MvcApplication_after/MvcApplication1/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MvcApplication1
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css")
+		            .Include("~/Content/styles.css")
+            .Include("~/Content/bootstrap.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/mordernizer")
+                .Include("~/Scripts/mordernizer-*")
+                );
+
+
+            bundles.Add(new ScriptBundle("~/bundles/js")
+                .Include("~/Scripts/jquery*")
+                .Include("~/Scripts/mustache.js")
+                .Include("~/Scripts/bootstrap.js")
+                );
+
+            BundleTable.EnableOptimizations = true;
+        }
+
+    }
+}
